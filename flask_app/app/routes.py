bebb50f8dd4825839426d360ca47b7f73e6bbf0d
AbdulMoezK <moezkhan05@gmail.com>
2025-11-08 18:57:43 -0500

    logging in update
    making it so user can log in with either their username or email

--- flask_app/app/routes.py
+++ flask_app/app/routes.py
@@ -19,7 +19,7 @@ def signup():
 @routes_bp.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
-    user = User.query.filter_by(email=data['email']).first()
+    user = User.query.filter_by(username=data['username']).first() or User.query.filter_by(email=data['username']).first()
     if user and user.check_password(data['password']):
         token = create_access_token(identity=str(user.id))  
         return jsonify({'token': token}), 200
