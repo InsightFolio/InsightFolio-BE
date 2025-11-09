817fd08707a55a9f12ebec037da62696010ebc1a
LiamHilliard <hilliardliam@outlook.com>
2025-11-09 14:34:23 -0500

    addressed feedback

--- flask_app/app/extensions.py
+++ flask_app/app/extensions.py
@@ -1,11 +1,6 @@
 from flask_SQLAlchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-from .models import User, Stock, Score, Log, Transaction
-
-from flask import Flask
-from config import Config
-from extensions import db, bcrypt, migrate
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
